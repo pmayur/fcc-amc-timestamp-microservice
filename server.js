f400c28c7d87238dab929cd4881bc7b0b7bced66
pmayur <thisispatil@gmail.com>
2020-12-03 00:40:48 +0530

    Add endpoint for date handling

--- server.js
+++ server.js
@@ -2,31 +2,68 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/api/timestamp", (req, res) => {
+    // get UTC date
+    let date = new Date().toUTCString();
 
-// your first API endpoint... 
+    res.json({
+        unix: new Date(date).getTime(),
+        utc: date,
+    });
+});
+
+app.get("/api/timestamp/:date", function (req, res) {
+    let requestedDate = req.params.date;
+
+    // parse into Integer if date received is a unix timestamp
+    let integerRequestedDate = parseInt(requestedDate);
+    let stringRequestedDate = integerRequestedDate.toString();
+
+    // if integer version of date equals string version of date timestamp is passed
+    let isTimeStampPassedAsDate = requestedDate === stringRequestedDate;
+
+    if (isTimeStampPassedAsDate) {
+        requestedDate = integerRequestedDate;
+    }
+
+    let date = new Date(requestedDate);
+
+    // Invalid Date is a NaN
+    if (date instanceof Date && !isNaN(date)) {
+        res.json({
+            unix: date.getTime(),
+            utc: date,
+        });
+    } else {
+        // response for all invalid dates
+        res.json({
+            error: new Date("").toString(),
+        });
+    }
+});
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    res.json({ greeting: "hello API" });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
